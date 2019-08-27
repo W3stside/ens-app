748c5c41012723f201878024cd9fe174e8ecd2b8
Velenir <velenir@gmail.com>
2019-08-27 10:54:49 +0300

    promt web3connect on load

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { ApolloProvider } from 'react-apollo'
 import 'core-js/es/object'
+
+import Web3Connect from 'web3connect'
+
 import App from 'App'
 import { setupENS } from '@ensdomains/ui'
 import { SET_ERROR } from 'graphql/mutations'
@@ -10,11 +13,33 @@ import { GlobalStateProvider } from 'globalState'
 import 'globalStyles'
 import { setupClient } from 'apolloClient'
 
+const setupWeb3Connect = async () => {
+  const web3Connect = new Web3Connect.Core()
+
+  const providerPromise = new Promise((resolve, reject) => {
+    // subscribe to connect
+    web3Connect.on('connect', provider => {
+      resolve(provider)
+    })
+
+    // subscribe to close
+    web3Connect.on('close', () => {
+      resolve()
+    })
+  })
+
+  web3Connect.toggleModal()
+
+  return providerPromise
+}
+
 window.addEventListener('load', async () => {
   let client
   try {
     client = await setupClient()
-    await setupENS({ reloadOnAccountsChange: true })
+
+    const customProvider = await setupWeb3Connect()
+    await setupENS({ reloadOnAccountsChange: true, customProvider })
   } catch (e) {
     console.log(e)
     await client.mutate({
